250dd05aafdb9b37eb35572dd2622a432f5b5e04
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2014-05-15 09:41:26 -0400

    Update stats.py

--- back-end/modules/analysis/stats/stats.py
+++ back-end/modules/analysis/stats/stats.py
@@ -41,11 +41,33 @@ SPECS = {
 				'result' : 'numeric'
 			}
 		}
+		#These are the specifications for the function you are creating.
+		"correlation" : {
+			#It expects two parameters, fields, which reference numeric "database columns".
+			#Remember, these specifications serve as a way to build our front-end (the part you see)
+			#dynamically. But I also included it here to give you a reference of what to build.
+			'param': {
+				'field_1': {
+					#This numeric field_reference type requests we get a numerical column from the database
+					#Hence we get an entire array of numbers returned (one per row)
+					'type' : 'field_reference numeric',
+					'comment': 'First field',
+				},
+				'field_2': {
+					'type' : 'field_reference numeric',
+					'comment': 'Second field'
+				}
+			},
+			#This entry_result says it expects to return a JSON object containing a numeric value named "correlation"
+			'entry_result': {
+				'correlation' : 'numeric'
+			}
+		}
 	}
 }
 
 def basic(working_set, param=False):
-	fieldVals = param['field'] #remember, this has been converted to an array of the referenced field values
+	fieldVals = param['field'] 
 	average = 0
 	total = 0
 	variance = 0
@@ -101,5 +123,13 @@ def binary_operation(working_set, param=False):
 			'result' : results
 		}
 	}
-def covariance(working_set, param):
+	
+#This is the function you are implementing
+def correlation(working_set, param):
+	#working_set is a reference to our current working data. It does not need to be used in this function.
+	
+	#param is a dictionary with the keys set to our parameters (see the SPECS variables) and the values set to
+	#our requested data. For example, param['field_1'] references our field_1 parameter. Notice (from the comments
+	#above, this is an array.
+	
 	pass
