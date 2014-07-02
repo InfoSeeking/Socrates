1edce7db1ad6c33cde9c65d3e2d3c62b687024a2
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2014-07-01 21:26:27 -0400

    PHP listener almost functional, removed tweepy

--- back-end/php/listener.php
+++ back-end/php/listener.php
@@ -12,9 +12,27 @@ require_once("lib/Toro.php");//Routing library
 
 header("Access-Control-Allow-Origin: *");
 
-class RootHandler {
-	public function get(){
-		echo "SOCRATES endpoint";
+function getParam($field, $DATA, $required=false, $default=null){
+    if (!isset($DATA[$field])) {
+        if ($required) {
+            throw new InvalidArgumentException(sprintf("Required field '%s' not passed", $field));
+        } else {
+            return $default;
+        }
+    }
+    return $DATA[$field];
+}
+
+class RunHandler {
+	public function get($type, $module, $fn){
+        $working_set_id = getParam("working_set_id", $_GET);
+        $return_all_data = getParam("return_all_data", $_GET) == "true" ? "return_all_data" : "";
+        $input = escapeshellarg(getParam("input", $_GET, false, "{}"));
+        $cmd = sprintf("cd ../ && python socrates_cli.py %s --input %s --run %s %s %s 2>&1", $return_all_data, $input, $type, $module, $fn);
+        echo $cmd;
+        $out = [];
+		exec($cmd, $out);
+        echo implode("<br/>", $out);
 	}
 }
 
@@ -26,7 +44,7 @@ class SpecHandler {
 }
 
 Toro::serve(array(
-	"/" => "RootHandler",
+	"/run/:alpha/:alpha/:alpha" => "RunHandler",
 	"/specs" => "SpecHandler"
 ));
 
--- docs/dev.php
+++ docs/dev.php
@@ -18,6 +18,7 @@ include("inc/top_2.php");
   			<li>TextBlob <a href="http://textblob.readthedocs.org/en/latest/install.html" target="_blank">http://textblob.readthedocs.org/en/latest/install.html</a></li>
   			<li>pymongo <a href="http://api.mongodb.org/python/current/installation.html" target="_blank">http://api.mongodb.org/python/current/installation.html</a></li>
   			<li>Flask <a href="http://flask.pocoo.org/docs/installation/" target="_blank">http://flask.pocoo.org/docs/installation/</a></li>
+  			<li>Tweepy<a href="https://github.com/tweepy/tweepy" target="_blank">https://github.com/tweepy/tweepy</a></li>
   		</ul>
   		<p>Afterwards, download the source from GitHub at <a href="https://github.com/kevinAlbs/Socrates" target="_blank">https://github.com/kevinAlbs/Socrates</a></p>
   		<p>The directories containing the code are front-end and back-end. Put the directory named front-end somewhere in your web root (i.e. htdocs or public_html). For example, my web root is located in my home directory in public_html in /home/kevin/public_html. So I created a subdirectory named socrates and moved the front-end directory there. The back-end directory can remain elsewhere.</p>
