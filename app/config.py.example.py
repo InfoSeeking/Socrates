f63f8209d915434c0d66ba930f2bda53ce67027e
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2016-05-09 21:51:13 -0400

    Add optional mongodb auth

--- app/config.py.example.py
+++ app/config.py.example.py
@@ -5,14 +5,22 @@ Fill in your values for oauth keys and rename this file to config.py (without .e
 
 #!/usr/bin/python
 CREDS = {
-	"reddit" : {
-		"client_id" : "",
-		"secret": "",
-		"uname": "",
-		"pass": ""
-	},
+    "reddit" : {
+        "client_id" : "",
+        "secret": "",
+        "uname": "",
+        "pass": ""
+    },
     "facebook_token" : "",
     "nyt_comment" : "",
     "nyt_article" : "",
     "YouTube_key" : ""
+    
+    # Uncomment the following if you want to use authentication
+    # with mongodb (highly recommended).
+    #
+    # "mongo" : {
+    #     "username": "",
+    #     "password": ""
+    # }
 }
--- app/user.py
+++ app/user.py
@@ -12,11 +12,15 @@ user_id = -1
 run_id = -1
 
 client = MongoClient()
-mongodb = client.socrates
+db = client.socrates
+
+if ('mongo' in config.CREDS):
+    db.authenticate(config.CREDS['mongo']['username'], config.CREDS['mongo']['password'], "admin")
+
 
 def authenticate(u, p):
     global username, user_id
-    result = mongodb.users.find_one({"username": u, "password": p})
+    result = db.users.find_one({"username": u, "password": p})
     if result:
         user_id = result['_id']
         return True
@@ -25,19 +29,19 @@ def authenticate(u, p):
 
 def register(u, p):
     # Check if user already exists
-    result = mongodb.users.find_one({"username": u, "password": p})
+    result = db.users.find_one({"username": u, "password": p})
     if result:
         return False
-    mongodb.users.insert_one({"username": u, "password": p})
+    db.users.insert_one({"username": u, "password": p})
     return True
 
 def getWorkingSet(working_set_id):
-    working_set = mongodb.working_set.find_one({"_id" : ObjectId(working_set_id)})
+    working_set = db.working_set.find_one({"_id" : ObjectId(working_set_id)})
     del working_set['_id']
     return working_set
 
 def getWorkingSets():
-    working_sets = list(mongodb.working_set.find({"user_id" : user_id}))
+    working_sets = list(db.working_set.find({"user_id" : user_id}))
     for i in xrange(len(working_sets)):
         working_set_id = working_sets[i]['_id']
         del working_sets[i]['_id']
@@ -47,28 +51,28 @@ def getWorkingSets():
 #returns the insert id
 def addWorkingSet(working_set):
     working_set['user_id'] = user_id
-    insert_id = mongodb.working_set.insert_one(working_set).inserted_id
+    insert_id = db.working_set.insert_one(working_set).inserted_id
     del working_set['_id']
     return insert_id
 
 def removeWorkingSet(working_set_id):
-    mongodb.working_set.remove({"_id" : ObjectId(working_set_id)});
+    db.working_set.remove({"_id" : ObjectId(working_set_id)});
 
 def updateWorkingSet(working_set_id, working_set):
     working_set["_id"] = ObjectId(working_set_id)
-    mongodb.working_set.save(working_set)
+    db.working_set.save(working_set)
     del working_set['_id']
 
 def renameWorkingSet(working_set_id, new_name):
     working_set = getWorkingSet(working_set_id)
     working_set["working_set_name"] = new_name
     working_set["_id"] = ObjectId(working_set_id)
-    mongodb.working_set.save(working_set)
+    db.working_set.save(working_set)
     del working_set['_id']
 
 def log_run(typ,mod,fn):
     global user_id, run_id
-    run_id = mongodb.log.insert_one({
+    run_id = db.log.insert_one({
         "namespace": "run",
         "type": typ,
         "module": mod,
@@ -79,7 +83,7 @@ def log_run(typ,mod,fn):
 #log any general activity (login, logout, etc.)
 def log_activity(name, value):
     global user_id, run_id
-    mongodb.log.insert_one({
+    db.log.insert_one({
         "namespace": "activity",
         "name": name,
         "value": value,
