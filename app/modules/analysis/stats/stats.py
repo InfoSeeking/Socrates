d53da07ba9584b6d9ec655e307f697a72e676608
SaraAnn Stanway <sarastanway@gmail.com>
2016-07-26 21:59:16 -0400

    fixed bugs in gen_math

--- app/modules/analysis/stats/stats.py
+++ app/modules/analysis/stats/stats.py
@@ -1,15 +1,26 @@
-#!/usr/bin/python
+import numeric_string_parser
 
-from __future__ import division
-from pyparsing import (Literal,CaselessLiteral,Word,Combine,Group,Optional,ZeroOrMore,Forward,nums,alphas,oneOf)
-import math
-import operator
-
-SPECS = {	'description' : 'Provides functions for statistical analysis',
+SPECS = {
+	'description' : 'Provides functions for statistical analysis',
 	'functions' : {
+		"gen_math" :{
+			'param_order': ['formula', 'xParam'],
+			'param': {
+				'formula': {
+					'type': 'text',
+					'comment': 'Expression to be evaluated',
+				},
+				'xParam': {
+					'type': 'field_reference numeric',
+					'comment': 'x values',
+				}
+			}
+		},
+	
+				
 		'basic' : {
 			'param': {
-				': {
+				'field': {
 					'type' : 'field_reference numeric',
 					'comment': 'Field to analyze',
 				}
@@ -45,21 +56,6 @@ SPECS = {	'description' : 'Provides functions for statistical analysis',
 				'result' : 'numeric'
 			}
 		},
-    "gen_math" : {
-      'param_order': ['formula', 'xParam'],
-      'param': {
-        'formula': {
-          'type': 'text'
-          'comment': 'Expression to be evaluated'
-          },
-          'xParam': {
-          'type': 'field_reference numeric',
-          'comment': 'x values',
-          }
-        }
-      }
-    },
-
 		"correlation" : {
 			'param': {
 				'field_1': {
@@ -95,6 +91,22 @@ SPECS = {	'description' : 'Provides functions for statistical analysis',
 	}
 }
 
+def gen_math(working_set, param=False):
+	parser = NumericStringParser()
+	results = []
+	xList = param['xParam']
+	expression = param['formula']
+	for x in range(len(xList)):
+		xValue = xList[x]
+		modified_formula = ''
+		for i in range (len(expression)):
+			if expression[i] != 'x':
+				modified_formula += expression[i]
+			else:
+				modified_formula += xValue
+		r=parser.eval(modified_formula)
+		results.append(r)
+	return results
 def basic(working_set, param=False):
 	fieldVals = param['field'] 
 	average = 0
@@ -152,117 +164,17 @@ def binary_operation(working_set, param=False):
 			'result' : results
 		}
 	}
-
-def gen_math (working_set, param=False):
-  class NumericStringParser(object):
-    def pushFirst(self, strg, loc, toks ):
-        self.exprStack.append( toks[0] )
-    def pushUMinus(self, strg, loc, toks ):
-        if toks and toks[0]=='-': 
-            self.exprStack.append( 'unary -' )
-    def __init__(self):
-        """
-        expop   :: '^'
-        multop  :: '*' | '/'
-        addop   :: '+' | '-'
-        integer :: ['+' | '-'] '0'..'9'+
-        atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'
-        factor  :: atom [ expop factor ]*
-        term    :: factor [ multop factor ]*
-        expr    :: term [ addop term ]*
-        """
-        point = Literal( "." )
-        e     = CaselessLiteral( "E" )
-        fnumber = Combine( Word( "+-"+nums, nums ) + 
-                           Optional( point + Optional( Word( nums ) ) ) +
-                           Optional( e + Word( "+-"+nums, nums ) ) )
-        ident = Word(alphas, alphas+nums+"_$")       
-        plus  = Literal( "+" )
-        minus = Literal( "-" )
-        mult  = Literal( "*" )
-        div   = Literal( "/" )
-        lpar  = Literal( "(" ).suppress()
-        rpar  = Literal( ")" ).suppress()
-        addop  = plus | minus
-        multop = mult | div
-        expop = Literal( "^" )
-        pi    = CaselessLiteral( "PI" )
-        expr = Forward()
-        atom = ((Optional(oneOf("- +")) +
-                 (pi|e|fnumber|ident+lpar+expr+rpar).setParseAction(self.pushFirst))
-                | Optional(oneOf("- +")) + Group(lpar+expr+rpar)
-                ).setParseAction(self.pushUMinus)       
-        # by defining exponentiation as "atom [ ^ factor ]..." instead of 
-        # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
-        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
-        factor = Forward()
-        factor << atom + ZeroOrMore( ( expop + factor ).setParseAction( self.pushFirst ) )
-        term = factor + ZeroOrMore( ( multop + factor ).setParseAction( self.pushFirst ) )
-        expr << term + ZeroOrMore( ( addop + term ).setParseAction( self.pushFirst ) )
-        # addop_term = ( addop + term ).setParseAction( self.pushFirst )
-        # general_term = term + ZeroOrMore( addop_term ) | OneOrMore( addop_term)
-        # expr <<  general_term       
-        self.bnf = expr
-        # map operator symbols to corresponding arithmetic operations
-        epsilon = 1e-12
-        self.opn = { "+" : operator.add,
-                "-" : operator.sub,
-                "*" : operator.mul,
-                "/" : operator.truediv,
-                "^" : operator.pow }
-        self.fn  = { "sin" : math.sin,
-                "cos" : math.cos,
-                "tan" : math.tan,
-                "abs" : abs,
-                "trunc" : lambda a: int(a),
-                "round" : round,
-                "sgn" : lambda a: abs(a)>epsilon and cmp(a,0) or 0}
-    def evaluateStack(self, s ):
-        op = s.pop()
-        if op == 'unary -':
-            return -self.evaluateStack( s )
-        if op in "+-*/^":
-            op2 = self.evaluateStack( s )
-            op1 = self.evaluateStack( s )
-            return self.opn[op]( op1, op2 )
-        elif op == "PI":
-            return math.pi # 3.1415926535
-        elif op == "E":
-            return math.e  # 2.718281828
-        elif op in self.fn:
-            return self.fn[op]( self.evaluateStack( s ) )
-        elif op[0].isalpha():
-            return 0
-        else:
-            return float( op )
-    def eval(self,num_string,parseAll=True):
-        self.exprStack=[]
-        results=self.bnf.parseString(num_string,parseAll)
-        val=self.evaluateStack( self.exprStack[:] )
-        return val
-	parser = NumericStringParser()
-results = []
-  for x in range (0, len(xParam)):
-    xValue = xParam[x]
-    modified_formula = ''
-    if formula[x] != 'x':
-      modified_formula += formula[x]
-    else:
-      modified_formula += xValue
-    r = parser.eval(modified_formula)
-    results.append(r)
-  return results
-
-def correlation (working_set, param=False):
-  field1Vals = param['field_1']
-  field2Vals = param['field_2']
-  sumX = 0
-  sumY = 0
-  sum X2 = 0
-  sum Y2 = 0
-  sumXY = 0
-  for i in rang(len(field1Vals)):
-    sumX += field1Vals[i]
+	
+def correlation(working_set, param=False):
+	field1Vals = param['field_1']
+	field2Vals = param['field_2']
+	sumX = 0
+	sumY = 0
+	sumX2 = 0
+	sumY2 = 0
+	sumXY = 0
+	for i in range(len(field1Vals)):
+		sumX += field1Vals[i]
 		sumY += field2Vals[i]
 		sumX2 += field1Vals[i]**2
 		sumY2 += field2Vals[i]**2
