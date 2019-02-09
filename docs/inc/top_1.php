b1db657d08d6bacdfb131e332dc7b981f7d2f74a
mmitsui <mmitsui@scarletmail.rutgers.edu>
2019-02-09 15:04:58 -0500

    Removed extra PHP files, Interface.js

--- docs/inc/top_1.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Documentation - SOCRATES</title>
-  <meta name="description" content="">
-  <link href='http://fonts.googleapis.com/css?family=Alegreya+Sans:400,700' rel='stylesheet' type='text/css'>
-  <link href="style.css" type="text/css" rel="stylesheet" />
-  <meta name="author" content="Kevin Albertson">
